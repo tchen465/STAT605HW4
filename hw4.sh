0e9cb7ad10c2c06eca85f501d1251e4ae2f663d4
tchen465 <147094751+tchen465@users.noreply.github.com>
2023-11-30 04:01:07 -0600

    Update hw4.sh

--- hw4.sh
+++ hw4.sh
@@ -2,13 +2,13 @@
 
 # Check for correct number of arguments
 if [ "$#" -ne 2 ]; then
-    echo "Usage: ./hw4.sh <template_spectrum> <data_directory_tgz>"
+    echo "Usage: ./hw4.sh <template_spectrum> <data_directory>"
     exit 1
 fi
 
 # Assign arguments to variables for clarity
 template_spectrum=$1
-data_directory_tgz=$2
+data_directory=$2
 
 # Untar R installation and FITSio package
 tar -xzf R413.tar.gz
@@ -20,10 +20,7 @@ export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
 
 # Unpack the provided .tgz file
-tar -xzf $data_directory_tgz
-
-# Extract the directory name from the .tgz file
-data_directory=${data_directory_tgz%.tgz}
+tar -xzf $data_directory.tgz
 
 # Run hw4.R script
 Rscript hw4.R $template_spectrum $data_directory
